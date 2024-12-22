a092fc0ff441f4ac799174b49089c7708257b854
Osleyder <osleyder.gonzalez@gmail.com>
2024-12-22 01:49:26 -0500

    Core/Initial: Initial Project
    Signed-off-by: Osleyder <osleyder.gonzalez@gmail.com>

---
+++ inmueble/src/main/java/com/osleyder/inmueble/InmuebleApplication.java
@@ -0,0 +1,13 @@
+package com.osleyder.inmueble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmuebleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmuebleApplication.class, args);
+	}
+
+}
---
+++ inmueble/src/test/java/com/osleyder/inmueble/InmuebleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osleyder.inmueble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmuebleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
